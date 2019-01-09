b16c7b90318706fa3ce6d0dc6a3df0fca812ae1e
SatishAkula <46445157+SatishAkula@users.noreply.github.com>
2019-01-09 17:55:32 +0530

    Update ThreadExample.java

--- Programing/ThreadExample.java
+++ Programing/ThreadExample.java
@@ -1,41 +1,43 @@
 class MyfirstThread extends Thread
 {
 	int array[];
-	static int i,sum=0;
+	static int i=0;
+	int sum=0;
 	MyfirstThread(int[] array)
 	{
 		this.array=array;
 	}
 	public void run()
 	{
-		for(i=0;i<10;i++) {
+		for(;i<10;i++) {
 			if(i%2==0)
 			{
 				sum=sum+array[i];
-				System.out.println(sum);
 			}
+			System.out.println(sum+"in one");
 		}
 		
 	}
 }
-
 class MysecondThread extends Thread
 {
 	int array[];
-	static int i,sum=0;
+	static int i=0;
+	int sum=0;
 	MysecondThread(int[] array)
 	{
 		this.array=array;
 	}
 	public void run()
 	{
-		for(i=0;i<10;i++) {
-			if(i%2==0)
+		for(;i<10;i++) {
+			if(i%2!=0)
 			{
 				sum=sum+array[i];
-				System.out.println(sum);
 			}
+			System.out.println(sum+"in two");
 		}
+		
 		
 	}
 }
@@ -43,11 +45,12 @@ public class ThreadExample {
 
 	public static void main(String args[])
 	{
+	long startTime = System.nanoTime();
 	int arr[]=new int[10];
 	int i;
 	for(i=0;i<10;i++)
 	{
-		arr[i]=arr[i]+1;
+		arr[i]=i+1;
 	}
 	MyfirstThread obj1=new MyfirstThread(arr);
 	Thread t1=new Thread(obj1);
@@ -56,5 +59,8 @@ public class ThreadExample {
 	MysecondThread obj2=new MysecondThread(arr);
 	Thread t2=new Thread(obj2);
 	t2.start();
+	long endTime   = System.nanoTime();
+	long totalTime = endTime - startTime;
+	System.out.println(totalTime);
 	}
 }
