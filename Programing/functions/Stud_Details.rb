775153ecbdcae4e3544ede821fec02bcf5dfac20
SatishAkula <46445157+SatishAkula@users.noreply.github.com>
2019-01-17 16:33:55 +0530

    Add files via upload

---
+++ Programing/functions/Stud_Details.rb
@@ -0,0 +1,9 @@
+def Stud_Details(*test)
+  name=test[0]
+  roll=test[1]
+  idno=test[2]
+  puts "name of the student is #{name}"
+  puts "roll no of the student #{roll}"
+  puts "id number of the student #{idno}"
+end
+Stud_Details("satish",22,130347)
